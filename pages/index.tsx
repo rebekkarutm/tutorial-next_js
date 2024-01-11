3aa30a69c1f13892d629eac09c1cfb23bfadb4f3
rebekkarutm <rmdottir@gmail.com>
2024-01-11 09:07:02 +0000

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export const getStaticProps: GetStaticProps = async () => {
   const feed = [
     {
       id: "1",
-      title: "Prisma is the perfect ORM for Next.js",
+      title: "Prisma for Next.js",
       content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
       published: false,
       author: {
